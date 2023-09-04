691380ef30b4ff76918da041f7b2f0755f0e2fa7
minjong kim <alswhddkdl@gmail.com>
2023-09-05 08:34:13 +0900

    [FEAT]: Toolbar 메뉴 클릭리스너 연결

--- app/src/main/java/com/sparta/youandme/view/main/ContactListFragment.kt
+++ app/src/main/java/com/sparta/youandme/view/main/ContactListFragment.kt
@@ -5,29 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.get
 import com.sparta.youandme.R
 import com.sparta.youandme.databinding.FragmentContactListBinding
+import com.sparta.youandme.extension.ContextExtension.toast
 
 
 class ContactListFragment : Fragment() {
     private var _binding: FragmentContactListBinding? = null
     private val binding get() = _binding!!
+    private val menuClickListener by lazy {
+        Toolbar.OnMenuItemClickListener { item ->
+            when(item?.itemId) {
+                R.id.list_view -> {
+                    // TODO 리니어레이아웃매니저로 설정
+                }
+
+                R.id.grid_view -> {
+                    // TODO 그리드레이아웃매니저로 설정
+                }
+            }
+            false
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentContactListBinding.inflate(layoutInflater)
         return binding.root
 
     }
 
     private fun initViews() = with(binding) {
-        toolBar.title = getString(R.string.app_name)
+        toolBar.run {
+            title = getString(R.string.app_name)
+            inflateMenu(R.menu.main_menu)
+            setOnMenuItemClickListener(menuClickListener)
+        }
     }
 
     override fun onDestroyView() {
