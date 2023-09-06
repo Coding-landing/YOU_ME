511b77acd097439a9523985cf1cf422b2be7fc3a
sunho_jeong <sunho512@gmail.com>
2023-09-06 19:08:34 +0900

    [FEAT] 메세지 버튼 클릭시 기본 메세지 앱 연결

--- app/src/main/java/com/sparta/youandme/view/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sparta/youandme/view/mypage/MyPageFragment.kt
@@ -1,26 +1,33 @@
 package com.sparta.youandme.view.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.sparta.youandme.R
 
 
 class MyPageFragment : Fragment() {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_my_page, container, false)
 
+        val myMessageImage = view.findViewById<ImageView>(R.id.my_message)
+
+        myMessageImage.setOnClickListener(View.OnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
+            startActivity(intent)
+        })
+
+        return view
+    }
     companion object {
         fun newInstance() = MyPageFragment()
     }
