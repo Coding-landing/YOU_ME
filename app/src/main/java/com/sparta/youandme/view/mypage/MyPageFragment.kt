f60b56073fd1c556c81b12b4d9b2a7c7422e3347
sunho_jeong <sunho512@gmail.com>
2023-09-07 11:12:48 +0900

    [FEAT] 전체적인 코드 정렬 깔끔해짐

--- app/src/main/java/com/sparta/youandme/view/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sparta/youandme/view/mypage/MyPageFragment.kt
@@ -17,84 +17,91 @@ import android.Manifest
 import com.sparta.youandme.R
 
 class MyPageFragment : Fragment() {
+
+    private lateinit var phoneNumberTextView: TextView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_my_page, container, false)
 
-        val phoneNumberTextView = view.findViewById<TextView>(R.id.my_phone_number)
-        val REQUEST_CALL_PHONE_PERMISSION = 1
-        val myCallImageView = view.findViewById<ImageView>(R.id.my_call)
-        myCallImageView.setOnClickListener {
-            val phoneNumber = phoneNumberTextView.text.toString()
+        phoneNumberTextView = view.findViewById(R.id.my_phone_number)
 
-            // 권한이 이미 허용되었는지 확인
-            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-                // 이미 권한이 허용된 경우, 전화 걸기 시도
-                val intent = Intent(Intent.ACTION_CALL)
-                intent.data = Uri.parse("tel:$phoneNumber")
-
-                startActivity(intent)
-            } else {
-                // 권한이 없는 경우, 권한 요청
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(Manifest.permission.CALL_PHONE),
-                    REQUEST_CALL_PHONE_PERMISSION
-                )
-            }
-        }
-
-
-        val myMessageImage = view.findViewById<ImageView>(R.id.my_message)
-
-        myMessageImage.setOnClickListener {
-            val intent = Intent(Intent.ACTION_MAIN)
-            intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
-            startActivity(intent)
-        }  // 기본 메세지 연결 버튼 구현
-
-        val myEmailImageView = view.findViewById<ImageView>(R.id.my_emailbt)
-
-        myEmailImageView.setOnClickListener {
-            val intent = Intent(Intent.ACTION_SENDTO).apply {
-                data = Uri.parse("mailto:")
-            }
-
-            intent.setPackage("com.google.android.gm")
-
-            try {
-                startActivity(intent)
-            } catch (e: ActivityNotFoundException) {
-                Toast.makeText(requireContext(), "Gmail 앱을 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
-            }
-        }  // emailbt 클릭시 기본 gmail이 실행되도록 연결 버튼 구현
-
-        val mySnsImageView = view.findViewById<ImageView>(R.id.my_snsbt)
-        val snsTextView = view.findViewById<TextView>(R.id.my_sns)
-
-        mySnsImageView.setOnClickListener {
-            val searchText = snsTextView.text.toString()
-
-            val intent = Intent(Intent.ACTION_WEB_SEARCH)
-            intent.putExtra("query", searchText)
-            startActivity(intent)
-        }  // snsbt 클릭시 sns의 텍스트를 인터넷과 연동해 사이트와 연결, 버튼 구현
-
-        val myblgoImageView = view.findViewById<ImageView>(R.id.my_blogbt)
-        val blogTextView = view.findViewById<TextView>(R.id.my_blog_adress)
-
-        myblgoImageView.setOnClickListener {
-            val searchText = blogTextView.text.toString()
-
-            val intent = Intent(Intent.ACTION_WEB_SEARCH)
-            intent.putExtra("query", searchText)
-            startActivity(intent)
-        }  // blogbt 클릭시 blog의 텍스트를 인터넷과 연동해 사이트와 연결, 버튼 구현
+        setupClickListeners(view)
 
         return view
+    }
+
+    private fun setupClickListeners(view: View) {
+        view.findViewById<ImageView>(R.id.my_message).setOnClickListener {
+            openMessagingApp()
+        }
+
+        view.findViewById<ImageView>(R.id.my_emailbt).setOnClickListener {
+            sendEmail()
+        }
+
+        view.findViewById<ImageView>(R.id.my_snsbt).setOnClickListener {
+            searchOnWeb(view.findViewById<TextView>(R.id.my_sns).text.toString())
+        }
+
+        view.findViewById<ImageView>(R.id.my_blogbt).setOnClickListener {
+            searchOnWeb(view.findViewById<TextView>(R.id.my_blog_adress).text.toString())
+        }
+
+        view.findViewById<ImageView>(R.id.my_call).setOnClickListener {
+            callPhoneNumber()
+        }
+    }
+
+    private fun openMessagingApp() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
+        startActivity(intent)
+    }
+
+    private fun sendEmail() {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+        }
+        intent.setPackage("com.google.android.gm")
+
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                requireContext(),
+                "Gmail 앱을 찾을 수 없습니다.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun searchOnWeb(query: String) {
+        val intent = Intent(Intent.ACTION_WEB_SEARCH)
+        intent.putExtra("query", query)
+        startActivity(intent)
+    }
+
+    private fun callPhoneNumber() {
+        val phoneNumber = phoneNumberTextView.text.toString()
+
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CALL_PHONE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$phoneNumber")
+            startActivity(intent)
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CALL_PHONE),
+                1 // REQUEST_CALL_PHONE_PERMISSION 변수 대신 직접 사용
+            )
+        }
     }
 
     companion object {
