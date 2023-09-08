9bb6cd403b46833a1b0b1d35a7f2b8e94b37eee2
regular1020 <pjg1020@kookmin.ac.kr>
2023-09-08 10:44:53 +0900

    [FIX] 오류 개선

--- app/src/main/java/com/sparta/youandme/view/addcontact/AddContactDialogFragment.kt
+++ app/src/main/java/com/sparta/youandme/view/addcontact/AddContactDialogFragment.kt
@@ -3,6 +3,7 @@ package com.sparta.youandme.view.addcontact
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -28,11 +29,13 @@ class AddContactDialogFragment : Fragment() {
     private lateinit var editsns: EditText
     private lateinit var editname: EditText
     private lateinit var editblog: EditText
-    private lateinit var editmbit :EditText
-    private lateinit var editnickname : EditText
+    private lateinit var editmbit: EditText
+    private lateinit var editnickname: EditText
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentAddContactDialogBinding.bind(view)
+        val context = requireContext()
         val btn = binding.diaBtnWhite
         editelnum = binding.diaEditTelNum
         editmail = binding.diaEditMail
@@ -41,7 +44,6 @@ class AddContactDialogFragment : Fragment() {
         editblog = binding.diaEditBlogAddress
         editmbit = binding.diaEditMbti
         editnickname = binding.diaEditNickname
-        val context = requireContext()
         editelnum.addTextChangedListener(createUserTextWatcher(editelnum))
         editmail.addTextChangedListener(createUserTextWatcher(editmail))
         editname.addTextChangedListener(createUserTextWatcher(editname))
@@ -53,8 +55,6 @@ class AddContactDialogFragment : Fragment() {
             ) {
                 context.toast("연락처 추가 성공!")
                 btn.isEnabled = true
-
-                val frag = ContactListFragment()
                 val callingobject = CallingObject(
                     id = UUID.randomUUID().toString(),
                     name = editname.text.toString(),
@@ -68,19 +68,31 @@ class AddContactDialogFragment : Fragment() {
                     type = -1
                 )
                 val bundle = Bundle().apply {
-                    putParcelable("model",callingobject)
+                    putParcelable("Ncontact", callingobject)
                 }
-                setFragment(this,bundle)
-            } else {
+               println(bundle)
+                setFragment(this, bundle)
+            } else if(!editelnum.text.toString().isEmpty() && !editname.text.toString()
+                .isEmpty() && !editmail.text.toString().isEmpty()){
                 context.toast("다시 확인해주세요.")
             }
-
-
+            else {
+            binding.diaBtnWhite.isEnabled = false
+                context.toast("다시 확인해주세요.")
+            }
         }
-
+        val cons2 = binding.diaCons2
+        val check = binding.diaCheckBox
+        check.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                cons2.visibility = View.VISIBLE
+            } else {
+                cons2.visibility = View.GONE
+            }
+        }
     }
 
-    private fun setFragment(frag : Fragment, bundle: Bundle) {
+    private fun setFragment(frag: Fragment, bundle: Bundle) {
         val activity = (requireActivity() as MainActivity)
         val viewPager = activity.findViewById<ViewPager2>(R.id.view_pager)
         parentFragmentManager.beginTransaction().remove(this).commit()
@@ -88,7 +100,7 @@ class AddContactDialogFragment : Fragment() {
         frag.onDestroy()
         viewPager.isVisible = true
         viewPager.setCurrentItem(0, false)
-        parentFragmentManager.setFragmentResult("callingobject",bundle)
+        parentFragmentManager.setFragmentResult("Bundle", bundle)
     }
 
     override fun onCreateView(
@@ -103,15 +115,16 @@ class AddContactDialogFragment : Fragment() {
     fun createUserTextWatcher(editText: EditText): TextWatcher {
         return object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
+                val context = requireContext()
                 val name = editname.text.toString()
                 val email = editmail.text.toString()
                 val number = editelnum.text.toString()
                 val numberPattern = "^01([016789])-?([0-9]{3,4})-?([0-9]{4})$".toRegex()
                 val isEmailValid = Patterns.EMAIL_ADDRESS.matcher(email).matches()
                 if (!number.isEmpty() && !email.isEmpty() && !name.isEmpty() && isEmailValid && number.matches(
-                        numberPattern)
+                        numberPattern
                     )
-                 {
+                ) {
                     binding.diaBtnWhite.isEnabled = true
                 } else {
                     binding.diaBtnWhite.isEnabled = false
@@ -141,11 +154,16 @@ class AddContactDialogFragment : Fragment() {
             override fun afterTextChanged(s: Editable?) {
                 val blog = editblog.text.toString()
                 val sns = editsns.text.toString()
-                val urlPattern = "^(https?://)?[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(\\/[a-zA-Z0-9/?=&-]*)?\$".toRegex()
-                if (blog.isEmpty() && sns.isEmpty() && blog.matches(urlPattern) && sns.matches(urlPattern)){
+                val urlPattern =
+                    "^(https?://)?[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(\\/[a-zA-Z0-9/?=&-]*)?\$".toRegex()
+                if (blog.isEmpty() && sns.isEmpty() && blog.matches(urlPattern) && sns.matches(
+                        urlPattern
+                    )
+                ) {
                     binding.diaBtnWhite.isEnabled = true
+                } else {
+                    binding.diaBtnWhite.isEnabled = false
                 }
-                else{binding.diaBtnWhite.isEnabled = false}
 
             }
 
