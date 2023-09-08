a134b387949f97be4c041de879e571c5712507b1
developShin <blueskyroad@naver.com>
2023-09-09 08:11:52 +0900

    [FEAT] : 메세지 버튼 눌렀을 떄 메세지 앱으로 이동 [FEAT] : 데이터 값 받아와서 textview에 입력 [DESIGN] : 안쪽 text색상 변경

--- app/src/main/java/com/sparta/youandme/view/detail/ContactDetailFragment.kt
+++ app/src/main/java/com/sparta/youandme/view/detail/ContactDetailFragment.kt
@@ -1,22 +1,50 @@
 package com.sparta.youandme.view.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import com.sparta.youandme.R
-
+import com.sparta.youandme.data.CallObjectData
+import com.sparta.youandme.databinding.FragmentCallBinding
+import com.sparta.youandme.databinding.FragmentContactDetailBinding
+import com.sparta.youandme.model.CallingObject
 
 class ContactDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentContactDetailBinding
+    private lateinit var data: CallingObject // CallingObject 타입으로 변경
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        data = CallObjectData.list.first()
+
+        binding = FragmentContactDetailBinding.bind(view)
+        binding.detailMessageBtn.setOnClickListener {
+            val phoneNumber = data.mobileNumber // data로 넘기면 될 듯함
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("sms:$phoneNumber")
+            startActivity(intent)
+        }
+
+        binding.detailMyName.text = data.name
+        binding.detailCallNum.text = data.mobileNumber
+        binding.detailSnsAddress.text = data.snsAddress
+        binding.detailEmail.text = data.email
+        binding.detailMbti.text = data.mbti
+        binding.detailNickName.text = data.nickName
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact_detail, container, false)
     }
 
