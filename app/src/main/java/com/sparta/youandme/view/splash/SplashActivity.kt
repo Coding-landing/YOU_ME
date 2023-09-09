dbf92cdf5fb7dfbc59aa165c8bb4189f9db36afa
phr0820 <thwn1012@gmail.com>
2023-09-09 22:47:42 +0900

    [FEAT]스플래쉬 화면 제작

---
+++ app/src/main/java/com/sparta/youandme/view/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.sparta.youandme.view.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.sparta.youandme.R
+import com.sparta.youandme.view.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(Runnable {
+            Intent(this, MainActivity::class.java).apply {
+                startActivity(this)
+                finish()
+            }
+        }, 3000) // 3초 후(3000) 스플래시 화면을 닫습니다
+    }
+}
