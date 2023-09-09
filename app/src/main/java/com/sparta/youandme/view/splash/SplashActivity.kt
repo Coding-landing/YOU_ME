75c3d2aae73f26b6c51dce43d1664a94507a2727
phr0820 <thwn1012@gmail.com>
2023-09-10 00:49:31 +0900

    [FEAT]스플래쉬 화면 제작 및 뒤로가기 버튼 추가

--- app/src/main/java/com/sparta/youandme/view/splash/SplashActivity.kt
+++ app/src/main/java/com/sparta/youandme/view/splash/SplashActivity.kt
@@ -1,25 +1,22 @@
 package com.sparta.youandme.view.splash
 
-import android.content.Intent
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sparta.youandme.R
-import com.sparta.youandme.view.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
-
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed(Runnable {
-            Intent(this, MainActivity::class.java).apply {
-                startActivity(this)
-                finish()
-            }
-        }, 3000) // 3초 후(3000) 스플래시 화면을 닫습니다
     }
+//    public fun Activity.installSplashScreen(): SplashScreen {
+//        val splashScreen = SplashScreen(this)
+//        splashScreen.install()
+//        return splashScreen
+//    }
 }
