779db68958f7919f79bb9bc559525db700596d4c
developShin <blueskyroad@naver.com>
2023-09-09 15:00:23 +0900

    [FEAT] : callbtn눌러서 callpage넘어가게 만듬

--- app/src/main/java/com/sparta/youandme/view/detail/ContactDetailFragment.kt
+++ app/src/main/java/com/sparta/youandme/view/detail/ContactDetailFragment.kt
@@ -3,43 +3,56 @@ package com.sparta.youandme.view.detail
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
+import androidx.viewpager2.widget.ViewPager2
 import com.sparta.youandme.R
-import com.sparta.youandme.data.CallObjectData
-import com.sparta.youandme.databinding.FragmentCallBinding
 import com.sparta.youandme.databinding.FragmentContactDetailBinding
 import com.sparta.youandme.model.CallingObject
+import com.sparta.youandme.view.main.MainActivity
 
 class ContactDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentContactDetailBinding
-    private lateinit var data: CallingObject
+    private var data: CallingObject? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        data = CallObjectData.list.first()
+        data = arguments?.getParcelable<CallingObject>("model")
+
 
         binding = FragmentContactDetailBinding.bind(view)
+
         binding.detailMessageBtn.setOnClickListener {
-            val phoneNumber = data.mobileNumber // data로 넘기면 될 듯함
+            val phoneNumber = data?.mobileNumber // data로 넘기면 될 듯함
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse("sms:$phoneNumber")
             startActivity(intent)
         }
 
-        binding.detailMyName.text = data.name
-        binding.detailCallNum.text = data.mobileNumber
-        binding.detailSnsAddress.text = data.snsAddress
-        binding.detailEmail.text = data.email
-        binding.detailMbti.text = data.mbti
-        binding.detailNickName.text = data.nickName
-        binding.detailPicture.setImageResource(data.imgId)
+        binding.detailCallBtn.setOnClickListener {
+
+            val activity =
+                (requireActivity() as MainActivity).findViewById<ViewPager2>(R.id.view_pager)
+            val bundle = Bundle().apply {
+                putParcelable("model", data)
+            }
+            this@ContactDetailFragment.onDestroy()
+            this@ContactDetailFragment.onDetach()
+            activity.setCurrentItem(2, true)
+            parentFragmentManager.setFragmentResult("callObject", bundle)
+
+        }
+
+        binding.detailMyName.text = data?.name
+        binding.detailCallNum.text = data?.mobileNumber
+        binding.detailSnsAddress.text = data?.snsAddress
+        binding.detailEmail.text = data?.email
+        binding.detailMbti.text = data?.mbti
+        binding.detailNickName.text = data?.nickName
+        binding.detailPicture.setImageURI(data?.imgId)
     }
 
     override fun onCreateView(
